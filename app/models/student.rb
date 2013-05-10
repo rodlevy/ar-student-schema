4e4495780ee61d22b84379ae83296f8ee8a4f616
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-09 19:49:20 -0500

    Mostly done, one test left

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,29 @@
 require_relative '../../db/config'
 
-# implement your Student model here
+class Student < ActiveRecord::Base
+
+
+   validates :email, :uniqueness => true
+   # validates Date.today - :birthday.years, :exclusion => { :in => [0,1,2,3]}
+   validates :age, :exclusion => { :in => (0..3)}
+   validates :email, :format => { :with => /[a-zA-Z1_9]+@[a-zA-Z1_9]+\.[a-zA-Z1_9]{2,}/}
+   # validates :should_have_at_least_10_numbers => true
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    now = Date.today
+    (now - birthday).div(365)  
+  end
+
+  def should_have_at_least_10_numbers
+    if 4 < 10
+      errors.add(:phone, "fail")
+    end
+  end
+
+end
+
+
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -4,6 +4,18 @@ require_relative '../config'
 
 class CreateStudents < ActiveRecord::Migration
   def change
-    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |student|
+       student.string :first_name
+       student.string :last_name
+       student.string :gender
+       student.date :birthday
+       student.string :email
+       student.string :phone
+     end
+
+
   end
 end
+
+
+# first_name,last_name,gender,birthday,email,phone
